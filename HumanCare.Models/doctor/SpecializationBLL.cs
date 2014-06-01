f726e22c26b90e9acb5eb9842e69c15ac5c966a1
milandashara <milandashara@gmail.com>
2014-06-01 22:36:45 +0800

    Merge branch 'master' of https://github.com/sakthie/HumanCare
    Conflicts:
    	HumanCare.suo

---
+++ HumanCare.Models/doctor/SpecializationBLL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Humancare.Data;
+namespace HumanCare.BLL.doctor
+{
+    public class SpecializationBLL
+    {
+        HealthCareNewEntities entities = new HealthCareNewEntities();
+
+        public Specialization getSpecialization(int id)
+        {
+            return entities.Specializations.Where(e=>e.id==id).SingleOrDefault();
+        }
+
+        public void delete(int id)
+        {
+            Specialization s=getSpecialization(id);
+            entities.Specializations.DeleteObject(s);
+            entities.SaveChanges();
+        }
+
+        public void save(Specialization s)
+        {
+            entities.Specializations.AddObject(s);
+            entities.SaveChanges();
+        }
+
+        public IEnumerable<Specialization> getSpecializations()
+        {
+            return entities.Specializations;
+        }
+    }
+}
---
+++ HumanCareManagementSystem/Areas/Doctor/Controllers/SpecializationController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity.Validation;
+using Humancare.Data;
+using HumanCare.BLL.doctor;
+
+
+namespace HumanCarePresentationLayer.Areas.Doctor.Controllers
+{
+    public class SpecializationController : Controller
+    {
+        //
+        // GET: /Doctor/Specialization/
+       
+        SpecializationBLL specializationBLL = new SpecializationBLL();
+        public ActionResult Index()
+        {
+            IEnumerable<Specialization> specilizations = specializationBLL.getSpecializations();
+            return View(specilizations);
+        }
+
+        //
+        // GET: /Doctor/Specialization/Details/5
+
+        public ActionResult Details(int id)
+        {
+            IEnumerable<Specialization> specilizations = specializationBLL.getSpecializations();
+            return View(specilizations);
+        }
+
+        //
+        // GET: /Doctor/Specialization/Create
+
+        public ActionResult Create()
+        {
+            Specialization s = new Specialization();
+            return View(s);
+        }
+
+        //
+        // POST: /Doctor/Specialization/Create
+
+        [HttpPost]
+        public ActionResult Create(Specialization specilization)
+        {
+            try
+            {
+                specializationBLL.save(specilization);
+                return RedirectToAction("Index");
+            }
+            catch(Exception e)
+            {
+                if (e.GetType() != typeof(DbEntityValidationException))
+                {
+                    if (this.HttpContext.IsDebuggingEnabled)
+                    {
+                        ModelState.AddModelError(string.Empty, e.ToString());
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty,"Some Technical error Happened");
+
+                    }
+                }
+                return View();
+            }
+        }
+
+        //
+        // GET: /Doctor/Specialization/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+           
+
+            return View(specializationBLL.getSpecialization(id));
+        }
+
+        //
+        // POST: /Doctor/Specialization/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                Specialization s = specializationBLL.getSpecialization(id);
+                UpdateModel(s);
+                specializationBLL.save(s);
+                return RedirectToAction("Index");
+            }
+            catch(Exception e)
+            {
+                if (e.GetType() != typeof(DbEntityValidationException))
+                {
+                    if (this.HttpContext.IsDebuggingEnabled)
+                    {
+                        ModelState.AddModelError(string.Empty, e.ToString());
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Some Technical error Happened");
+
+                    }
+                }
+                return View();
+            }
+        }
+
+        //
+        // GET: /Doctor/Specialization/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+
+            return View(specializationBLL.getSpecialization(id));
+        }
+
+        //
+        // POST: /Doctor/Specialization/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, Specialization specialization)
+        {
+            try
+            {
+                specializationBLL.delete(id);
+                
+
+                return RedirectToAction("Index");
+            }
+            catch(Exception e)
+            {
+                if (e.GetType() != typeof(DbEntityValidationException))
+                {
+                    if (this.HttpContext.IsDebuggingEnabled)
+                    {
+                        ModelState.AddModelError(string.Empty, e.ToString());
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Some Technical error Happened");
+
+                    }
+                }
+                return View();
+            }
+        }
+    }
+}
